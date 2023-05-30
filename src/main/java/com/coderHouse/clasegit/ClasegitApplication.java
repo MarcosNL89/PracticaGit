3d7152974a92b950a76fa5a727f359ebd0dc9d8e
MarcosNLute <lutemarcos@gmail.com>
2023-05-30 10:11:09 -0300

    Project initialization

---
+++ src/main/java/com/coderHouse/clasegit/ClasegitApplication.java
@@ -0,0 +1,13 @@
+package com.coderHouse.clasegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasegitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasegitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderHouse/clasegit/ClasegitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderHouse.clasegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasegitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
