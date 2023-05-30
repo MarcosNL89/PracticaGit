ee754b91636a70b85cfe4ec304eb8dda94b488ba
MarcosNLute <lutemarcos@gmail.com>
2023-05-30 10:38:57 -0300

    Added end point Hola. Error no conecta. To do: Fix endpoint

--- src/main/java/com/coderHouse/clasegit/ClasegitApplication.java
+++ src/main/java/com/coderHouse/clasegit/ClasegitApplication.java
@@ -1,13 +1,19 @@
 package com.coderHouse.clasegit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClasegitApplication {
+public class ClasegitApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClasegitApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(ClasegitApplication.class, args);
+
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Server listining http://localhost:3000/h2-console");
+
+	}
 }
